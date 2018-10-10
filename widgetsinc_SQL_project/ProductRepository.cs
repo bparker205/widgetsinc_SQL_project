59e47393b0e5174a732cfe7d8cfdb852bc96d7a6
Brian <brian@theparkerbunch.com>
2018-10-09 21:50:39 -0500

    Added AddProduct,UpdateProduct & DeleteProduct methods

--- widgetsinc_SQL_project/ProductRepository.cs
+++ widgetsinc_SQL_project/ProductRepository.cs
@@ -37,11 +37,11 @@ namespace widgetsinc_SQL_project
                 while (reader.Read())
                 {
                     Product prod = new Product();
-                    // prod.id = (int)reader["id"];
-                    // prod.brand_id = (int)reader["bId"];
+                    //prod.id = (int)reader["id"];
+                    //prod.brand_id = (int)reader["brand_id"];
                     prod.description = reader["description"].ToString();
-                    // prod.status_id = (int)reader["sId"];
-                    // prod.catagory_id = (int)reader["cId"];
+                    //prod.status_id = (int)reader["status_id"];
+                    //prod.catagory_id = (int)reader["catagory_id"];
                     products.Add(prod);
                 }
 
@@ -51,5 +51,58 @@ namespace widgetsinc_SQL_project
             }
 
         }
+
+        // AddProduct method adds a record in product table
+        public void AddProduct(int bID, string desc, int sID, int catID)
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO product (brand_id, description, status_id, catagory_id) VALUES (@bID,@desc,@sID,@catID);";
+                cmd.Parameters.AddWithValue("@bID", bID);
+                cmd.Parameters.AddWithValue("@desc", desc);
+                cmd.Parameters.AddWithValue("@sID", sID);
+                cmd.Parameters.AddWithValue("catID", catID);
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+        }
+
+
+        // UpdateProduct method updates record in product table
+        public void UpdateProduct(int ID, string Desc, int StatusId)
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "UPDATE product SET Desc = @desc, StatusId = sID WHERE ID = @id ;";
+                cmd.Parameters.AddWithValue("@desc", Desc);
+                cmd.Parameters.AddWithValue("@sID", StatusId);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+
+        // DeleteProduct method deletes record in product table
+        public void DeleteProduct(int ID)
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM product WHERE id = @ID;";
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
